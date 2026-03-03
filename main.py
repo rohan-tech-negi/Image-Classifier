96fbf7c4809ca19206c77b926e8db766b5a76ddc
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-04 00:07:07 +0530

    started the day with making an image recogonition model

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-import cv2
+import cv2 as cv2
+import numpy as numpy
+import matplotlib.pyplot as plt 
+from tensorflow.keras import datasets, layers, models
+
+(training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
+
+training_images, training_images = training_images / 255 , training_images / 255
+
+
+class_names = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
