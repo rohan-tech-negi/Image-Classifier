3628e802fa723aec3cf56ac9fc38881b08ae9045
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-04 00:49:17 +0530

    training the model

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from tensorflow.keras import datasets, layers, models
 
 (training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
 
-training_images, training_images = training_images / 255 , training_images / 255
+training_images = training_images / 255.0
+testing_images = testing_images / 255.0
 
 
 class_names = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
@@ -39,3 +40,9 @@ model.add(layers.Dense(10, activation='softmax'))
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images, testing_labels))
+
+loss, accuracy = model.evaluate(testing_images, testing_labels)
+print(f"Loss: {loss}")
+print(f"Accuracy: {accuracy}")
+
+model.save('image_classifier.model')
